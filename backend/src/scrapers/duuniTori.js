99e971009793b994e3f8f7644bc01795bc67ff8d
Saed Abukar <saed.a.abukar@gmail.com>
2024-10-08 02:07:45 +0300

    test4

--- backend/src/scrapers/duuniTori.js
+++ backend/src/scrapers/duuniTori.js
@@ -1,5 +1,7 @@
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const fs = require("fs");
+const path = require("path");
 
 puppeteer.use(StealthPlugin());
 
